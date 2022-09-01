385333db4f0033142c2ea70bb72eeff270b7beca
GabrielOlivaa <gedfto@gmail.com>
2022-09-01 14:32:34 -0300

    exercicios ok

---
+++ exercicio1/script.js
@@ -0,0 +1,7 @@
+const array1= [22,03,97,14,05]
+const array2= ["gol","celta","ka","uno","fusca"]
+const array3=[22, "fusca",12,true,"jeep"]
+
+console.log(` lista de array 01(numeros): ${array1} 
+lista de array 02(string): ${array2} 
+lista de array 03(mix string-number-booleano): ${array3}`)
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,18 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+
+const array1= [22,03,97,14,05]
+const array2= ["gol","celta","ka","uno","fusca"]
+const array3=[22, "fusca",12,true,"jeep"]
+
+console.log(` lista de array 01(numeros): ${array1} 
+lista de array 02(string): ${array2} 
+lista de array 03(mix string-number-booleano): ${array3}`)
+
+console.log("quantidade de itens do array 01:",array1.length)
+console.log( "quantidade de itens do array 02:",array2.length)
+console.log("quantidade de itens do array 03:",array3.length)
+
+console.log("primeira lista:",array1[0],"segunda lista:",array2[1],"terceira lista:",array3[2])
+
+console.log("checagem de booleano primeira lista:",array1.includes(22))
+console.log("checagem da booleano terceira lista:",array3.includes(14))
